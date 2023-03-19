c045d2e9a966a8f3f1b9cf0d8da92aacecf43db1
aurora <109873714+9-class@users.noreply.github.com>
2023-03-19 10:20:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-It worked
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login/Signup Page</title>
+</head>
+<body>
+	<h1>Welcome to our site!</h1>
+	<p>Please choose one of the following options:</p>
+	<ul>
+		<li><a href="login.html">Login</a></li>
+		<li><a href="signup.html">Signup</a></li>
+	</ul>
+</body>
+</html>
